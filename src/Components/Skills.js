7289c9be19e17b9a9d744eacbbd53f6249d0e70f
GabssssMes <messner92@gmail.com>
2023-08-01 19:33:38 +0200

    added sass to skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -7,6 +7,7 @@ import vscode from "../Images/skills/icons8-vs-code-480.png";
 import jest from "../Images/skills/icons8-jest-can-collect-code-coverage-information-from-entire-projects-96.png";
 import linux from "../Images/skills/icons8-linux-96.png";
 import git from "../Images/skills/icons8-git-480.png";
+import sass from "../Images/skills/sass.png";
 
 let Skills = [];
 
@@ -52,6 +53,11 @@ Skills.push({
 Skills.push({
   title: "Git",
   src: git,
+});
+
+Skills.push({
+  title: "SASS",
+  src: sass,
 });
 
 export { Skills };
